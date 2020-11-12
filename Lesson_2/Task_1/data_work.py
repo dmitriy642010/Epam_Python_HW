a241c5cdf336480458b13fe8b3c9282ca010ecd7
dmitriy642010 <dmitriy642010@mail.ru>
2020-11-12 15:35:21 +0300

    new

--- Lesson_2/Task_1/data_work.py
+++ Lesson_2/Task_1/data_work.py
@@ -1,27 +1,31 @@
 from typing import List
+import string
 
 
-"""
+def data_open(file_path):
+    with open(file_path) as f:
+        file_path = bytes(f.read(), "ascii").decode("unicode-escape")
+        return file_path
+
+
+# print(data_open("data1.txt"))
+
+
 def get_longest_diverse_words(file_path: str) -> List[str]:
 
+    file = data_open(file_path)
+    exclude = set(string.punctuation)
+    text = ''.join(ch for ch in file if ch not in exclude)
+    data_set = text.split()
+    data_set = sorted(data_set, key=lambda x: (-len(set(x)), -len(x)))
 
-    with open('data.txt') as f:        
-        file_path = f.read()
-
-    
-    #result = ''.join(c for c in file_path if c.isalpha())
-
-    #longest_word =  max(file_path, key=len)
-    #return longest_word
-   
-
-    
-    #words = max(file_path, key=lambda x: len(x)) 
-    
-
+    return data_set[:10]
 
 
 print(get_longest_diverse_words("data.txt"))
+
+
+"""
 
 
 
@@ -37,6 +41,5 @@ def count_non_ascii_chars(file_path: str) -> int:
     ...
 
 
-def get_most_common_non_ascii_char(file_path: str) -> str:
-
-"""
+def get_most_common_non_ascii_char(file_path: str) -> str:  
+    """
