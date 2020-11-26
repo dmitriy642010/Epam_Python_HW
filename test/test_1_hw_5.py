2d0632675f1a9320b1aa4c534b236acae96b30d4
dmitriy642010 <dmitriy642010@mail.ru>
2020-11-26 20:09:40 +0300

    task_5 new

---
+++ test/test_1_hw_5.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+
+from Epam_training_HW.hw.hw_5_task_1 import Homework, Student, Teacher
+
+import pytest
+
+
+@pytest.fixture()
+def student():
+    return Student("Roman", "Petrov")
+
+
+@pytest.fixture()
+def teacher():
+    return Teacher("Daniil", "Shadrin")
