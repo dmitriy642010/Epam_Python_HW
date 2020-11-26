f17ef130591735422b2eb0da7cb922a60bf62655
dmitriy642010 <dmitriy642010@mail.ru>
2020-11-26 17:59:44 +0300

    new

--- hw/hw_2_task_1.py
+++ hw/hw_2_task_1.py
@@ -15,7 +15,6 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
     text = ''.join(ch for ch in file if ch not in exclude)
     data_set = text.split()
     data_set = sorted(data_set, key=lambda x: (len(set(x)), len(x)))
-
     return data_set[:10]
 
 
@@ -28,24 +27,14 @@ def get_rarest_char(file_path: str) -> str:
         else:
             d[elem] += 1
     minimum = min(d.values())
-    for num, count in d.items():
-        if count == minimum:
-            return num
-
-
-print(get_rarest_char("char.txt"))
+    res = [str(num) for num, count in d.items() if count == minimum]
+    return res[0]
 
 
 def count_punctuation_chars(file_path: str) -> int:
-    ch = []
     file = data_open(file_path)
-
-    for i in file:
-        if i in string.punctuation:
-            ch.append(i)
-    result = len(ch)
-
-    return result
+    res = [len(i) for i in file if i in string.punctuation]
+    return sum(res)
 
 
 def count_non_ascii_chars(file_path: str) -> int:
