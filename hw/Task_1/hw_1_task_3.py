4e30f47a22659d2e5a650e3d1b1737a707521d0d
Dmitriy Ivanov <60974317+dmitriy642010@users.noreply.github.com>
2020-12-03 12:19:26 +0300

    Delete hw_1_task_3.py

--- hw/Task_1/hw_1_task_3.py
+++
@@ -1,5 +0,0 @@
-def find_maximum_and_minimum(numbers: str):
-    with open(numbers, "r") as f:
-        nums = f.read().splitlines()
-    result = sorted(nums)
-    return int(result[0]), int(result[-1])
