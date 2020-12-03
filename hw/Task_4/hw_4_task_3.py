aafbac2a4266cc7d52cb1a6a80c5a50d18c24815
Dmitriy Ivanov <60974317+dmitriy642010@users.noreply.github.com>
2020-12-03 12:20:59 +0300

    Delete hw_4_task_3.py

--- hw/Task_4/hw_4_task_3.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-def my_precious_logger(text: str):
-    if text.startswith("error"):
-        print(text, file=sys.stderr, end="")
-    else:
-        print(text, file=sys.stdout, end="")
